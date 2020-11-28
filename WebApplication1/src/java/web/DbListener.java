295f6221bfc93ddefdb33d0a56503ad9e8a784ad
rogerioalex2019PG <51092332+rogerioalex2019PG@users.noreply.github.com>
2020-11-28 14:12:02 -0300

    ex2

--- WebApplication1/src/java/web/DbListener.java
+++ WebApplication1/src/java/web/DbListener.java
@@ -5,6 +5,7 @@
  */
 package web;
 
+import br.edu.ads.poo.Disciplina;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.sql.*;
@@ -27,6 +28,53 @@ public static Connection getConnection() throws Exception{
         Connection con = null;
         Statement stmt = null;
         try{
+            Class.forName(CLASS_NAME);
+            con = DriverManager.getConnection(DB_URL);
+            stmt = con.createStatement();
+            stmt.execute(Disciplina.getCtreateStatement());
+            if(Disciplina.getList().isEmpty()){
+                stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'POO',"
+                        + "'Conceitos e evolução da tecnologia de orientação a objetos.',"
+                        + "'4',"
+                        + "10);");
+                 stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'bd',"
+                        + "'Banco de dados Conceitos de Base de Dados.',"
+                        + "'10',"
+                        + "4);");
+                  stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'si',"
+                        + "'Requisitos de segurança de aplicações, de base de dados e de comunicações.',"
+                        + "'10',"
+                        + "4);");
+                   stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'ESTAGIO',"
+                        + "'Estágio Aplicar os conhecimentos teóricos adquiridos no curso',"
+                        + "'10',"
+                        + "4);");
+                    stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'so2',"
+                        + "'Apresentação de um sistema operacional',"
+                        + "'10',"
+                        + "4);");
+                     stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'metodologia',"
+                        + "'O Papel da ciência e da tecnologia.',"
+                        + "'10',"
+                        + "4);");
+                     stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'LP4',"
+                        + "'Comandos de linguagens usadas na construção e estruturação de sites para a Web',"
+                        + "'10',"
+                        + "4);");
+                     stmt.execute("INSERT INTO disciplinas VALUES("
+                        + "'es3',"
+                        + "'Conceitos, evolução e importância de arquitetura de software.',"
+                        + "'10',"
+                        + "4);");
+            }
+                  
             
         }catch(Exception ex){
             
